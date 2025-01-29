8d419ff69d230b5b474768609f1f82e7ecd20207
racousin <racousin@student.42.fr>
2025-01-29 08:17:07 +0100

    add agent heuristic and fix init import

--- beergame/__init__.py
+++ beergame/__init__.py
@@ -1,6 +1,6 @@
 """Beer Game environment package"""
 
-from beergame.beergame.env.beergame import raw_env, env
+from beergame.env.beergame import raw_env, env
 from beergame.beergame_v0 import beergame_v0
 
 __all__ = ["beergame_v0", "raw_env", "env"]
