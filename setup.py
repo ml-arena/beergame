f130f913d8f4ace0459f04a124c98f7650963478
racousin <racousin@student.42.fr>
2025-01-29 10:26:32 +0100

    include png files

--- setup.py
+++ setup.py
@@ -2,9 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="beergame",
-    version="0.4",
+    version="0.6",
     description="Beer Game Environment for PettingZoo",
     packages=find_packages(),
+    package_data={
+        'beergame.env': ['*.png'],
+    },
     install_requires=[],
     python_requires=">=3.7",
 )
